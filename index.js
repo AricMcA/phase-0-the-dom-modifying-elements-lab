9123d56bfa2bc021f87d508ff8291cb537f869d4
Aric McAlister <aricmca@gmail.com>
2021-05-31 12:57:02 -0500

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code here!
+document.querySelector("#main").remove();
+
+const newHeader = document.createElement("h1");
+
+document.body.appendChild(newHeader);
+
+newHeader.id = "victory";
+
+document.querySelector("h1#victory")
+
+newHeader.innerHTML = "<h1>victory</h1> <p>Aric is the champion</p>";
